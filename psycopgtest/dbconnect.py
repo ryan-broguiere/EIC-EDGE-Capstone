965b1d806a8d3c7689e9352a9ec99cc1d2a33102
Nicole Nguyen <nnguy099@ucr.edu>
2023-01-24 23:34:02 +0000

    embed sql into python

--- psycopgtest/dbconnect.py
+++ psycopgtest/dbconnect.py
@@ -1,4 +1,5 @@
 import psycopg2
+import mysql.connector
 
 #pyscopg2 - this is a library that helps me connect to a postgres database
 #from Python
@@ -17,6 +18,32 @@ cursor = conn.cursor()
 #this is a sanity check - to verfiy that we are indeed connected to postgres:
 cursor.execute('SELECT * FROM information_schema.tables')
 
+# connectsql = mysql.connector.connect(user='postgres', database='capstone')
+# cursor = connectsql.cursor()
+
+# cursor.execute(
+#     "CREATE TABLE address ("
+#         "ID SERIAL PRIMARY KEY,"
+#         "Address VARCHAR(50),"
+#         "City VARCHAR(25),"
+#         "State VARCHAR(25),"
+#         "ZipCode VARCHAR(5)"
+#     ");"
+
+#     "CREATE TABLE customer ("
+#         "ID SERIAL PRIMARY KEY,"
+#         "FirstName VARCHAR(25),"
+#         "LastName VARCHAR(25),"
+#         "AddressID INT REFERENCES address(ID),"
+#         "Email VARCHAR(50)"
+#     ");"
+
+#     "CREATE TABLE account ("
+#         "ID SERIAL PRIMARY KEY,"
+#         "AccountNumber VARCHAR(10),"
+#         "CustomerID INT REFERENCES customer(ID),"
+#         "CurrentBalance FLOAT"
+#     ");")
 
 #now that you have a cursor, you can insert into your DB, retrieve, etc..
 
